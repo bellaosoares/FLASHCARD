fd567756030d9c6277b17c2b23b2ecdbdf43a0c1
bellaosoares <isabella.soares.siqueira@escola.pr.gov.br>
2025-07-06 17:50:35 -0300

    88

---
+++ assets/perguntas.js
@@ -0,0 +1,54 @@
+criaCartao(
+    'Geografia',
+    'Qual e o menor e o maior pais do mundo?',
+    'Vaticano e Russia.'
+)
+
+criaCartao(
+    'Historia',
+    'Quem foi o primeiro presidente do Brasil?',
+    'Marechal Deodoro da Fonseca.'
+)
+
+criaCartao(
+    'Ingles',
+    'Qual e o significado, em portugues, da palavra butterfly?',
+    'Borboleta.'
+)
+
+criaCartao(
+    'Fisica',
+    'A que temperatura a agua ferve?',
+    '100C.'
+)
+
+criaCartao(
+    'Biologia',
+    'Quantos ossos temos no nosso corpo?',
+    '206 ossos.'
+)
+
+criaCartao(
+    'Astrologia',
+    'Qual e o planeta mais proximo do Sol?',
+    'Mercurio.'
+)
+
+criaCartao(
+    'Arte',
+    'Quem pintou Mona Lisa?',
+    'Leonardo da Vinci.'
+)
+
+criaCartao(
+    'Quimica',
+    'Qual o nome popular do cloreto de sodio?',
+    'Sal de cozinha.'
+)
+
+criaCartao(
+    'Matematica',
+    'Qual e o resultado de 10-6x2?',
+    '-2.'
+)
+
